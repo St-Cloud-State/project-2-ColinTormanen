fcf4ee92b4b32e277aec8950a42d6e5295c400a6
ColinTormanen <144750228+ColinTormanen@users.noreply.github.com>
2024-11-13 17:09:37 +0000

    11/13/24

--- Warehouse/Context.java
+++ Warehouse/Context.java
@@ -1,5 +1,8 @@
 
 public class Context {
+    public static final int IsUser = 1;
+    public static final int IsClerk = 2;
+    public static final int IsManager = 3;
     private static int currentState;
     private static Warehouse warehouse;
     private static Context context;
@@ -20,8 +23,53 @@ public class Context {
 
     public JFrame getFrame() { return frame; }
 
-    public static void main(String[] args) {
+    private Context() {
+        warehouse = Warehouse.instance();
 
+        states = new WarehouseState[4];
+        states[0] = LoginState.instance();
+        states[1] = UserState.instance();
+        states[2] = ClerkState.instance();
+        states[3] = ManagerState.instance();
+        nextState = new int[4][4];
+        nextState[0][0] = -1; nextState[0][1] = 1; nextState[0][2] = 2; nextState[0][3] = 3; 
+        nextState[1][0] = 0; nextState[1][1] = -2; nextState[1][2] = 2; nextState[1][3] = -2; 
+        nextState[2][0] = 0; nextState[2][1] = 1; nextState[2][2] = -2; nextState[2][3] = 3; 
+        nextState[3][0] = 0; nextState[3][1] = -2; nextState[0][2] = 2; nextState[3][3] = -2; 
+
+        currentState = 0;
+        frame = new JFrame("Warehouse GUI");
+	    frame.addWindowListener(new WindowAdapter()
+            { public void windowClosing(WindowEvent e) { System.exit(0); } });
+        frame.setSize(400,400);
+        frame.setLocation(400, 400);
+    }
+
+    public void changeState(int next) {
+        currentState = states[currentState][next];
+        if(currentState == -2) {
+            System.out.println("An error has occured");
+            terminate();
+        }
+        else if(currentState == -1) terminate();
+        states[currentState].run();
+    }
+
+    private void terminate() {
+        System.out.println("Exiting");
+        System.exit(0);
+    }
+
+    public static Context instace() {
+        if(context == null) context = new Context();
+        return context;
+    }
+
+
+
+    public static void main(String[] args) {
+        Context.instace();
+        states[currentState].run();
     }
 
 }
--- Warehouse/LoginState.java
+++ Warehouse/LoginState.java
@@ -6,15 +6,59 @@ public class LoginState extends WarehouseState implements ActionListener {
     private static LoginState instance;
     private AbstractButton userButton, clerkButton, managerButton, logoutButton;
 
+    // Private constructor
     private LoginState() {
-
+        super();
     }
 
+    // Get the instance of the state
     public static LoginState instance() {
         if(instance == null) instance = new LoginState();
         return instance;
     }
 
+    // Button press actions
+    public void actionPerformed(ActionEvent event) {
+        if(event.getSource().equals(this.userButton)) this.user();
+        else if(event.getSource().equals(this.clerkButton)) this.clerk();
+        else if(event.getSource().equals(this.managerButton)) this.manager();
+        else if(event.getSource().equals(this.logoutButton)) (Context.instance()).changeState(0);
+    }
+
+    // Clear the JFrame
+    public void clear() {
+        frame.getContentPane().removeAll();
+        frame.paint(frame.getGraphics());   
+    }
+
+    // Called if the user button is pressed
+    private void user() {
+        String id = JOptionPane.showInputDialog(frame,"Please input the user id: ");
+        if(Warehouse.instance().searchForClient(id) != null) {
+            (Context.instance()).setLogin(Context.IsUser);
+            (Context.instance()).setUser(id);
+            clear();
+            (Context.instance()).changeState(1);
+        }
+        else JOptionPane.showMessageDialog(frame,"Invalid user id.");
+        
+    }
+
+    // Called if the clerk button is pressed
+    private void clerk() {
+        (Context.instance()).setLogin(Context.IsClerk);
+        clear();
+        (Context.instance()).changeState(2);
+    }
+
+    // Called if the manager button is pressed
+    private void manager() {
+        (Context.instance()).setLogin(Context.IsManager);
+        clear();
+        (Context.instance()).changeState(3);
+    }
+
+    // Run the state
     public void run(){
         frame = Context.instance().getFrame();
         frame.getContentPane().removeAll();
